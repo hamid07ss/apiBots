6c3b5304caab8fee047b519b2b170e0f4f99c54d
Hamid <h.sarani75@gmail.com>
2017-08-27 15:24:55 +0430

    remove print

--- invite.php
+++ invite.php
@@ -42,7 +42,6 @@ try {
     // Handle telegram getUpdate request
     do {
         $data = $telegram->handleGetUpdates(100, 0);
-        print_r(count($data->getResult()));
         if(count($data->getResult())) {
             foreach ((array) $data->getResult() as $result) {
                 $bot->handleMessages($result);
