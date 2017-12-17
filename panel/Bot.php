a536709fb50d1f97514a1e42b8712dd6d272fd41
Hamid <h.sarani75@gmail.com>
2017-12-17 16:44:18 +0330

    fix bug

--- panel/Bot.php
+++ panel/Bot.php
@@ -307,11 +307,13 @@ class Bot {
 
         try {
             $redis = new Client();
-            $redis_ = new Client($parameters = array(
+
+            $parameters = array(
                 'host'     => '127.0.0.1',
                 'port'     => 6379,
                 'database' => 1
-            ));
+            );
+            $redis_ = new Client($parameters);
 
 
             if($result->getMessage()){
