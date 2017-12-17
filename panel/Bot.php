c28e2e04fff8dc0fecb4b4429f8e94a8f7f70e05
Hamid <h.sarani75@gmail.com>
2017-12-17 16:51:16 +0330

    fix bug

--- panel/Bot.php
+++ panel/Bot.php
@@ -488,6 +488,7 @@ class Bot {
                             foreach($Bots as $bot) {
                                 $botName = basename($bot);
                                 preg_match_all('/bot-(.*).session/', $botName, $botNum);
+                                print_r($botNum[1][0]);
                                 $DBName = $this->DBs["SuperGroups"] . $botNum[1][0];
 
                                 $Links[$botNum[1][0]] = $redis_->scard($DBName);
