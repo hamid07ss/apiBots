f764d80eb832ed372da062d51c2c7e59bc2caa94
Hamid <h.sarani75@gmail.com>
2018-12-26 02:20:11 +0330

    add proxy bot

--- proxy/Bot.php
+++ proxy/Bot.php
@@ -176,7 +176,7 @@ class Bot
                     'text' => 'Send',
                     'callback_data' => json_encode([
                         'action' => "SendProxy",
-                        'link' => "tg://proxy?server=single.ir-pro-tel.us&port=1425&secret=dd3fafdea89ab71e46a207"
+                        'link' => "tg://proxy?server=single.ir-pro-tel.us"
                             //$this->createProxyLink($proxyP),
 //                        'server' => $proxyP['server'],
 //                        'secret' => $proxyP['secret']
